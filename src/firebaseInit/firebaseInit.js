028d48978304a65bce4286666e28f8ac9123ccb8
sungjong <tjdwhd3345@gmail.com>
2021-09-05 00:12:58 +0900

    firebase commit

--- src/firebaseInit/firebaseInit.js
+++ src/firebaseInit/firebaseInit.js
@@ -1,5 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  browserSessionPersistence,
+  getAuth,
+  GoogleAuthProvider,
+  setPersistence,
+  signInWithPopup,
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,9 +20,10 @@ const firebase = initializeApp(firebaseConfig);
 
 getAuth().useDeviceLanguage();
 export const auth = getAuth();
-
+// 로그인상태 브라우저 세션으로 지속
+setPersistence(auth, browserSessionPersistence);
 const provider = new GoogleAuthProvider();
-// provider.seteCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
 export default firebase;
