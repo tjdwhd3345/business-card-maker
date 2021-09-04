b2845ed3de5979eb72c6962998f61ae7cbc03b59
sungjong <tjdwhd3345@gmail.com>
2021-09-05 00:16:07 +0900

    intro 는 폴더위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import LoginForm from './components/loginForm/LoginForm';
 import { auth, signInWithGoogle } from './firebaseInit/firebaseInit';
-import Intro from './main/intro';
+import LoginForm from './components/loginForm/LoginForm';
+import Intro from './components/main/Intro';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className={styles.App}>
       <BrowserRouter>
@@ -14,12 +15,12 @@ function App() {
             <LoginForm auth={auth} signInWithGoogle={signInWithGoogle} />
           </Route>
           <Route path='/main' exact>
-            <Intro />
+            <Intro auth={auth} />
           </Route>
         </Switch>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
